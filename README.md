3ad4c4e259acaff8b691dfae66dace9a5eea3901
Luciano Scarpulla <scarpulla53@gmail.com>
2024-04-21 18:47:28 +0200

    First draft

---
+++ README.md
@@ -0,0 +1,37 @@
+# Airport Dashboard
+
+Basic airport dashboard that helps filtering and visualizing airport data.
+
+## Data
+All data is coming from [OurAirports.com](OurAirports.com)
+
+## How to install (with [Rye](https://rye-up.com/))
+
+- Clone the repository
+
+```bash
+git clone 
+```
+- Use Rye to setup a virtual environment
+
+```bash
+rye sync
+```
+
+- Activate the environemnt 
+
+```bash
+source ./venv/bin/activate
+```
+
+- Run streamlit
+
+```bash
+streamlit run airport-catchment/app.py
+```
+
+
+# Dashboard
+
+![](static/dash.png)
+![](static/airport_map.png)
---
+++ airport-dashboard/app.py
@@ -0,0 +1,76 @@
+import streamlit as st
+
+import pydeck as pdk
+import pandas as pd
+
+
+@st.cache_data
+def get_airports():
+    return pd.read_csv("https://davidmegginson.github.io/ourairports-data/airports.csv")
+
+
+@st.cache_data
+def get_runways():
+    return pd.read_csv("https://davidmegginson.github.io/ourairports-data/runways.csv")
+
+
+runways = get_runways()
+airports = get_airports()
+
+st.title("Airport Netork")
+
+airport_type = st.multiselect("Airport Type", airports.type.unique())
+
+
+rwy_length = st.slider(
+    "Runway Length:",
+    runways.length_ft.min(),
+    runways.length_ft.max(),
+    runways.length_ft.mean(),
+    format="%d ft",
+)
+
+elevation = st.slider(
+    "Elevation",
+    airports.elevation_ft.min(),
+    airports.elevation_ft.max(),
+    airports.elevation_ft.mean(),
+    format="%d ft",
+)
+
+selected_airports = airports.loc[
+    (airports.id.isin(runways.loc[runways.length_ft <= rwy_length, "airport_ref"]))
+    & (airports.elevation_ft <= elevation)
+    & (airports.type.isin(airport_type))
+]
+
+
+st.dataframe(selected_airports, hide_index=True)
+
+airports_chart = pdk.Layer(
+    "ScatterplotLayer",
+    selected_airports,
+    get_position=["longitude_deg", "latitude_deg"],
+    auto_highlight=True,
+    get_radius=10_000,  # Radius is given in meters
+    get_fill_color=[255, 255, 200, 140],  # Set an RGBA value for fill
+    pickable=True,
+)
+
+
+st.metric(
+    label="Airports selected",
+    value=f"{selected_airports.shape[0]:,}",
+)
+
+mean_lat = selected_airports.latitude_deg.mean()
+mean_lon = selected_airports.longitude_deg.mean()
+
+view_state = pdk.ViewState(
+    longitude=mean_lon,
+    latitude=mean_lat,
+    zoom=3,
+)
+
+
+st.pydeck_chart(pdk.Deck(layers=[airports_chart], initial_view_state=view_state))
